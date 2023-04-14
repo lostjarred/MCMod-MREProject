76dc67ac2c2defb33fcfdfeadf2cb1883961fa38
lostjarred <58093147+lostjarred@users.noreply.github.com>
2023-04-15 08:11:58 +1000

    add mre drink item and food item

---
+++ src/main/java/com/lost/mreproject/ItemFoodDrinkBase.java
@@ -0,0 +1,23 @@
+package com.lost.mreproject;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.EnumAction;
+import net.minecraft.item.ItemStack;
+
+public class ItemFoodDrinkBase extends ItemFoodBase {
+
+	public ItemFoodDrinkBase(int amount, float saturation, String name) {
+		super(amount, saturation, false, name);
+	}
+	
+	@Override
+	public EnumAction getItemUseAction(ItemStack stack){
+		return EnumAction.DRINK;
+	}
+	
+	@Override
+	public ItemFoodDrinkBase setCreativeTab(CreativeTabs tab) {
+		super.setCreativeTab(tab);
+		return this;
+	}
+}
--- src/main/java/com/lost/mreproject/ModItems.java
+++ src/main/java/com/lost/mreproject/ModItems.java
@@ -6,15 +6,19 @@ import net.minecraftforge.registries.IForgeRegistry;
 
 public class ModItems {
 	public static ItemFoodBase mreItem = new ItemFoodBase(8, 0.8f, false, "mrefooditem").setCreativeTab(CreativeTabs.FOOD);
-	
+	public static ItemFoodDrinkBase mredrinkfood = new ItemFoodDrinkBase(8, 0.8f, "mredrinkfooditem").setCreativeTab(CreativeTabs.FOOD);
+	public static ItemBase mredrinkitem = new ItemBase("mredrinkitem").setCreativeTab(CreativeTabs.FOOD);
 	
 	public static void register(IForgeRegistry<Item> registry) {
 		registry.register(mreItem);
-		
+		registry.register(mredrinkfood);
+		registry.register(mredrinkitem);
 	}
 
 	public static void registerModels() {
 		mreItem.registerItemModel();
+		mredrinkfood.registerItemModel();
+		mredrinkitem.registerItemModel();
 	}
 
 }
