36889f3eb9057315fddce161b3c5eca9c686fa3f
lostjarred <58093147+lostjarred@users.noreply.github.com>
2023-05-02 13:40:16 +1000

    change variable name, rename item

--- src/main/java/com/lost/mreproject/ModItems.java
+++ src/main/java/com/lost/mreproject/ModItems.java
@@ -5,20 +5,20 @@ import net.minecraft.item.Item;
 import net.minecraftforge.registries.IForgeRegistry;
 
 public class ModItems {
-	public static ItemFoodBase mreItem = new ItemFoodBase(8, 0.8f, false, "mrefooditem").setCreativeTab(CreativeTabs.FOOD);
-	public static ItemFoodDrinkBase mredrinkfood = new ItemFoodDrinkBase(8, 0.8f, "mredrinkfooditem").setCreativeTab(CreativeTabs.FOOD);
-	public static ItemBase mredrinkitem = new ItemBase("mredrinkitem").setCreativeTab(CreativeTabs.FOOD);
+	public static ItemFoodBase MREITEMFOOD = new ItemFoodBase(8, 0.8f, false, "mreitemfood").setCreativeTab(CreativeTabs.FOOD);
+	public static ItemFoodDrinkBase MREITEMFOODDRINK = new ItemFoodDrinkBase(8, 0.8f, "mreitemfooddrink").setCreativeTab(CreativeTabs.FOOD);
+	public static ItemBase MISC = new ItemBase("misc").setCreativeTab(CreativeTabs.FOOD);
 	
 	public static void register(IForgeRegistry<Item> registry) {
-		registry.register(mreItem);
-		registry.register(mredrinkfood);
-		registry.register(mredrinkitem);
+		registry.register(MREITEMFOOD);
+		registry.register(MREITEMFOODDRINK);
+		registry.register(MISC);
 	}
 
 	public static void registerModels() {
-		mreItem.registerItemModel();
-		mredrinkfood.registerItemModel();
-		mredrinkitem.registerItemModel();
+		MREITEMFOOD.registerItemModel();
+		MREITEMFOODDRINK.registerItemModel();
+		MISC.registerItemModel();
 	}
 
 }
