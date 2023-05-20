69e6bce62b9fe509205fe3f1d4df83489b0f227b
lostjarred <58093147+lostjarred@users.noreply.github.com>
2023-05-20 10:44:50 +1000

    add temp coffee item for testing

--- src/main/java/com/lost/mreproject/ModItems.java
+++ src/main/java/com/lost/mreproject/ModItems.java
@@ -1,6 +1,7 @@
 package com.lost.mreproject;
 
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraftforge.registries.IForgeRegistry;
 
@@ -8,17 +9,21 @@ public class ModItems {
 	public static ItemFoodBase MREITEMFOOD = new ItemFoodBase(8, 0.8f, false, "mreitemfood").setCreativeTab(CreativeTabs.FOOD);
 	public static ItemFoodDrinkBase MREITEMFOODDRINK = new ItemFoodDrinkBase(8, 0.8f, "mreitemfooddrink").setCreativeTab(CreativeTabs.FOOD);
 	public static ItemBase MISC = new ItemBase("misc").setCreativeTab(CreativeTabs.FOOD);
+	//temp for testing
+	public static ItemFoodDrinkReturnItem COFFEE = new ItemFoodDrinkReturnItem(8, 0.8f, "coffee", Items.GLASS_BOTTLE);
 	
 	public static void register(IForgeRegistry<Item> registry) {
 		registry.register(MREITEMFOOD);
 		registry.register(MREITEMFOODDRINK);
 		registry.register(MISC);
+		registry.register(COFFEE);
 	}
 
 	public static void registerModels() {
 		MREITEMFOOD.registerItemModel();
 		MREITEMFOODDRINK.registerItemModel();
 		MISC.registerItemModel();
+		COFFEE.registerItemModel();
 	}
 
 }
