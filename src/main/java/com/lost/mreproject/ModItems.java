24a6eee2914318a9f7a080c2a38366da4c81846f
lostjarred <58093147+lostjarred@users.noreply.github.com>
2023-05-27 10:04:17 +1000

    rename mreitemfooddirnk to mredrink

--- src/main/java/com/lost/mreproject/ModItems.java
+++ src/main/java/com/lost/mreproject/ModItems.java
@@ -7,21 +7,20 @@ import net.minecraftforge.registries.IForgeRegistry;
 
 public class ModItems {
 	public static ItemFoodBase MREITEMFOOD = new ItemFoodBase(8, 0.8f, false, "mreitemfood").setCreativeTab(MreProject.MREFOODS_TAB);
-	public static ItemFoodDrinkBase MREITEMFOODDRINK = new ItemFoodDrinkBase(8, 0.8f, "mreitemfooddrink").setCreativeTab(MreProject.MREFOODS_TAB);
+	public static ItemFoodDrinkBase MREDRINK = new ItemFoodDrinkBase(8, 0.8f, "mredrink").setCreativeTab(MreProject.MREFOODS_TAB);
 	public static ItemBase MISC = new ItemBase("misc").setCreativeTab(MreProject.MREFOODS_TAB);
-	//temp for testing
 	public static ItemFoodDrinkReturnItem MREDRINK2 = new ItemFoodDrinkReturnItem(8, 0.8f, "mredrink2", Items.GLASS_BOTTLE).setCreativeTab(MreProject.MREFOODS_TAB);
 	
 	public static void register(IForgeRegistry<Item> registry) {
 		registry.register(MREITEMFOOD);
-		registry.register(MREITEMFOODDRINK);
+		registry.register(MREDRINK);
 		registry.register(MISC);
 		registry.register(MREDRINK2);
 	}
 
 	public static void registerModels() {
 		MREITEMFOOD.registerItemModel();
-		MREITEMFOODDRINK.registerItemModel();
+		MREDRINK.registerItemModel();
 		MISC.registerItemModel();
 		MREDRINK2.registerItemModel();
 	}
