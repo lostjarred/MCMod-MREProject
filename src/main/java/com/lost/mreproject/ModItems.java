fcd2b9e5e76a6cea04722530ca889f578720b77e
lostjarred <58093147+lostjarred@users.noreply.github.com>
2023-05-31 16:47:16 +1000

    adjust food values

--- src/main/java/com/lost/mreproject/ModItems.java
+++ src/main/java/com/lost/mreproject/ModItems.java
@@ -7,9 +7,9 @@ import net.minecraftforge.registries.IForgeRegistry;
 
 public class ModItems {
 	public static ItemFoodBase MREFOOD = new ItemFoodBase(8, 0.8f, false, "mrefood").setCreativeTab(MreProject.MREFOODS_TAB);
-	public static ItemFoodDrinkBase MREDRINK = new ItemFoodDrinkBase(8, 0.8f, "mredrink").setCreativeTab(MreProject.MREFOODS_TAB);
+	public static ItemFoodDrinkBase MREDRINK = new ItemFoodDrinkBase(5, 0.8f, "mredrink").setCreativeTab(MreProject.MREFOODS_TAB);
 	public static ItemBase MISC = new ItemBase("misc").setCreativeTab(MreProject.MREFOODS_TAB);
-	public static ItemFoodDrinkReturnItem MREDRINK2 = new ItemFoodDrinkReturnItem(8, 0.8f, "mredrink2", Items.GLASS_BOTTLE).setCreativeTab(MreProject.MREFOODS_TAB);
+	public static ItemFoodDrinkReturnItem MREDRINK2 = new ItemFoodDrinkReturnItem(3, 0.8f, "mredrink2", Items.GLASS_BOTTLE).setCreativeTab(MreProject.MREFOODS_TAB);
 	
 	public static void register(IForgeRegistry<Item> registry) {
 		registry.register(MREFOOD);
