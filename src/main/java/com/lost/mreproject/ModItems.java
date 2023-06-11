da2b751f38acb35d50943093f56be6c9f7bfe5b6
lostjarred <58093147+lostjarred@users.noreply.github.com>
2023-06-11 13:22:05 +1000

    Update ModItems.java
    update coffee to use new class with potion effects

--- src/main/java/com/lost/mreproject/ModItems.java
+++ src/main/java/com/lost/mreproject/ModItems.java
@@ -1,14 +1,17 @@
 package com.lost.mreproject;
 
 import net.minecraft.init.Items;
+import net.minecraft.init.MobEffects;
 import net.minecraft.item.Item;
+import net.minecraft.potion.PotionEffect;
 import net.minecraftforge.registries.IForgeRegistry;
 
 public class ModItems {
 	public static ItemFoodBase MREFOOD = new ItemFoodBase(8, 0.8f, false, "mrefood").setCreativeTab(MreProject.MREFOODS_TAB);
 	public static ItemFoodDrinkBase MREDRINK = new ItemFoodDrinkBase(5, 0.8f, "mredrink").setCreativeTab(MreProject.MREFOODS_TAB);
 	public static ItemBase MISC = new ItemBase("misc").setCreativeTab(MreProject.MREFOODS_TAB);
-	public static ItemFoodDrinkReturnItem MREDRINK2 = new ItemFoodDrinkReturnItem(3, 0.8f, "mredrink2", Items.GLASS_BOTTLE).setCreativeTab(MreProject.MREFOODS_TAB);
+	//20 tic, 1 second
+	public static ItemFoodDrinkReturnItemEffect MREDRINK2 = new ItemFoodDrinkReturnItemEffect(3, 0.8f, "mredrink2", Items.GLASS_BOTTLE, new PotionEffect(MobEffects.HASTE, 600)).setCreativeTab(MreProject.MREFOODS_TAB);
 	
 	public static void register(IForgeRegistry<Item> registry) {
 		registry.register(MREFOOD);
